f8c91470a590478564511a0907165c1ae5595c41
Case <case@doocom.cn>
2024-07-10 16:32:01 +0800

    feat: 特性: 🚀 新增webpack打包

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,16 @@
-// Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
 module.exports = {
-  apps: [
-    {
-      name: 'front-gateway',
-      script: 'dist/apps/front-gateway/main.js',
-      instances: 'max',
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '1G',
-      env: {
-        RUNNING_ENV: 'prod',
+	apps: [
+		{
+			name: 'front-gateway',
+			script: 'build/index.js',
+			instances: 'max',
+			autorestart: true,
+			watch: false,
+			max_memory_restart: '1G',
+			env: {
+				RUNNING_ENV: 'prod',
 				PORT: 3020,
-      },
-    },
-  ],
+			},
+		},
+	],
 };
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const CleanWebpackPlugin = require('clean-webpack-plugin').CleanWebpackPlugin;
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin').TsconfigPathsPlugin;
+// const SpeedMeasurePlugin = require('speed-measure-webpack-plugin');
+
+const tsConfigFile = 'tsconfig.build.json';
+
+// const swp = new SpeedMeasurePlugin();
+
+/**
+ * @type {import('webpack').Configuration}
+ */
+const config = {
+	mode: 'production',
+	target: 'node',
+	entry: path.resolve(__dirname, 'apps/front-gateway/src', 'main.ts'), // 入口文件。改成你自己的,
+	output: {
+		path: path.resolve(__dirname, 'build'), // 出口文件，如果没什么特殊的就可以不管,
+		filename: 'index.js',
+	},
+	// 忽略依赖
+	externals: [nodeExternals()],
+	plugins: [new CleanWebpackPlugin()],
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				loader: "esbuild-loader"
+			},
+		],
+	},
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js'],
+		plugins: [
+			// 别名路径处理
+			new TsconfigPathsPlugin({
+				configFile: tsConfigFile,
+			}),
+		],
+	},
+};
+
+// module.exports = swp.wrap(config);
+model.exports = config;
